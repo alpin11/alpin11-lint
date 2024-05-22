5f40a8868b5641c0da36b640232241190414630b
David Höck <d.hoeck@alpin11.com>
2024-05-22 15:12:22 +0200

    add eslint-config-love

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = {
     "extends": [
         "plugin:react/recommended",
         "plugin:react-hooks/recommended",
-        "standard-with-typescript",
+        "eslint-config-love",
         "plugin:react/jsx-runtime"
     ],
     "globals": {
